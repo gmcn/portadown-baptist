b1c2354ce582008a5044c51484cb910e544e3cef
Web Dev <webdev@webs-mbp.home>
2020-04-15 15:08:35 +0100

    policy page

---
+++ wp-content/themes/starting-theme/inc/page-policies/content.php
@@ -0,0 +1,9 @@
+<?php the_content(); ?>
+
+<div class="container back">
+  <div class="row">
+    <div class="col-md-6">
+      <a href="/">Back to home</a>
+    </div>
+  </div>
+</div>
---
+++ wp-content/themes/starting-theme/page-privacy-policy.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Privacy Policy Page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Starting_Theme
+ */
+
+get_header(); ?>
+
+
+<?php
+
+include(locate_template("inc/page-elements/breadcrumbs.php"));
+include(locate_template("inc/page-policies/content.php"));
+
+?>
+
+
+<?php
+get_footer();
